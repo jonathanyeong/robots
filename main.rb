eeea52be8b988434f91e920ec70685c879597006
Jonathan Yeong <jonathanyeong@gmail.com>
2015-04-21 18:50:16 +1000

    Instead of taking user input read commands from file
    This makes testing a whole bunch of commands much faster. It also means
    I can submit the testing data.

--- main.rb
+++ main.rb
@@ -1,13 +1,29 @@
 require_relative "robot"
+require "pry"
 
-while true
-  command = gets.chomp
-  if command.eql?("exit")
-    break
-  elsif command.eql?("help")
-    puts <<-DOC 
-      Move your robot around the board. Commands:
-        exit - Exit the program
-    DOC
+@robot = Robot.new()
+i = 0
+ARGF.each do |command|
+  #puts "#{i}: #{command}"
+  if (command.include?("PLACE"))
+    command.slice!("PLACE ")
+    args = command.split(',')
+    x = args[0].to_i
+    y = args[1].to_i
+    direction = args[2].gsub(/\s+/, "")
+    valid = @robot.place(x, y, direction)
+    if (!valid.eql?(true))
+      puts "Placement was invalid"
+      break 
+    end
+  elsif (command.include?("MOVE"))
+    @robot.move
+  elsif (command.include?("LEFT"))
+    @robot.rotate(0)
+  elsif (command.include?("RIGHT"))
+    @robot.rotate(1)
+  elsif (command.include?("REPORT"))
+    puts "OUTPUT: #{@robot.report}"
   end
+  i += 1 
 end
