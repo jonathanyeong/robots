d82019645e158a148876d4bd62f25625fb44a54c
Jonathan Yeong <jonathanyeong@gmail.com>
2015-04-21 17:46:50 +1000

    Remove board class from robot
    Trying to start with a simple solution first. Which is why we took out
    this board class call. It was unecessary because the board class doesn't
    do anything but store the pos of the robot. This pos can be easily
    tracked by the robot itself. My feelings of this may change in the
    future.
    
    Other changes:
    - Make the if statement more rubyish
    - Ensure that if an invalid "f" is given, we say it's an invalid place.

--- robot.rb
+++ robot.rb
@@ -1,29 +1,32 @@
 require_relative "board"
 
 class Robot
-  attr_accessor :pos, :direction, :board
-  def initialize(board)
+  attr_accessor :pos, :direction
+  def initialize()
     @pos = nil
     @direction = nil
-    @board = board
+    @width = 5
+    @height = 5
   end
 
   def place(x, y, f)
     is_valid = false
+    direction = f.upcase
     # Check if it's in the bounds of the board
-    if (x < @board.width and x >= 0 and y < @board.height and y >= 0)
+    if (x < @width and x >= 0 and y < @height and y >= 0)
       @pos = [x, y]
-
-      if (f == "NORTH")
-        @direction = [1, 0]
-      elsif (f == "EAST")
-        @direction = [0, 1]
-      elsif (f == "SOUTH")
-        @direction = [-1, 0]
-      elsif (f == "WEST")
-        @direction = [0, -1]
-      end
       is_valid = true
+      if (direction.eql?("NORTH"))
+        @direction = [1, 0]
+      elsif (direction.eql?("EAST"))
+        @direction = [0, 1]
+      elsif (direction.eql?("SOUTH"))
+        @direction = [-1, 0]
+      elsif (direction.eql?("WEST"))
+        @direction = [0, -1]
+      else
+        is_valid = false
+      end
     end
     is_valid  # Return is valid
   end
