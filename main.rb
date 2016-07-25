7819e6b3a8ee712cadf3967a3831019a7487ebf9
Jonathan Yeong <jonathanyeong@gmail.com>
2016-07-26 08:43:56 +1000

    Move commands from main into robot
    We don't want the main file performing actions on the robot. It is an
    interface to the robot and all it needs to do is give it commands. The
    robot will work out the rest.

--- main.rb
+++ main.rb
@@ -1,33 +1,15 @@
-require_relative "robot"
+require_relative 'robot'
+require_relative 'robot_factory'
 
-@robot = Robot.new(5, 5)
-robot_valid = false
-ARGF.each do |command|
-  if (command.include?("PLACE"))
-    arg_string = command.gsub!(/\s+/, "").split("PLACE")
-    args = arg_string.last.split(",")
-    x = args[0].to_i
-    y = args[1].to_i
-    direction = args[2]
-    robot_valid = @robot.place(x, y, direction)
-    if (robot_valid.eql?(false))
-      puts "#{command} -- Placement was invalid"
-    end
-  elsif (command.include?("MOVE"))
-    if robot_valid.eql?(true)
-      @robot.move
-    end
-  elsif (command.include?("LEFT"))
-    if robot_valid.eql?(true)
-      @robot.rotate(0)
-    end
-  elsif (command.include?("RIGHT"))
-    if robot_valid.eql?(true)
-      @robot.rotate(1)
-    end
-  elsif (command.include?("REPORT"))
-    if robot_valid.eql?(true)
-      puts "OUTPUT: #{@robot.report}"
+def main(*cmds)
+  robot = Robot.new(5, 5)
+  report = ''
+  cmds.each do |cmd|
+    report = robot.command(cmd)
+    if report == false
+      report = "#{cmd} -- Placement was invalid"
+      break
     end
   end
+  report
 end
